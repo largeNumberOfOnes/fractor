88b2da4b92a4b00d97af4caa6ea388de825f9830
largeNumberOfOnes <shchekinov2314@rambler.ru>
2025-12-02 01:52:25 +0300

    Add types.h

---
+++ swsrc/include/share/types.h
@@ -0,0 +1,22 @@
+#include <cstddef>
+#include <inttypes.h>
+#include <type_traits>
+
+using int8  = int8_t;
+using int16 = int16_t;
+using int32 = int32_t;
+using int64 = int64_t;
+
+using uint8  = uint8_t;
+using uint16 = uint16_t;
+using uint32 = uint32_t;
+using uint64 = uint64_t;
+
+using float32 = float;  // DEV [not garantued]
+using float64 = double; // DEV [not garantued]
+
+using byte  = std::byte;
+using usize = uint64;
+
+constexpr usize EXPANDABLE_ARRAY = 0;
+
