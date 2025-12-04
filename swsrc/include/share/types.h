0e30a0ef70078893f0acde51bd857963ebbdfee8
largeNumberOfOnes <shchekinov2314@rambler.ru>
2025-12-05 00:51:30 +0300

    Update types in share/types.h

--- swsrc/include/share/types.h
+++ swsrc/include/share/types.h
@@ -1,9 +1,10 @@
 #ifndef TYPES_HEADER
 #define TYPES_HEADER
 
-#include <cstddef>
-#include <inttypes.h>
 #include <type_traits>
+#include <inttypes.h>
+#include <cstddef>
+
 #include <gmpxx.h>
 
 using int8  = int8_t;
@@ -16,13 +17,16 @@ using uint16 = uint16_t;
 using uint32 = uint32_t;
 using uint64 = uint64_t;
 
-using float32 = float;  // DEV [not garantued]
-using float64 = double; // DEV [not garantued]
+using float32 = float;
+using float64 = double;
+static_assert(sizeof(float32) == 4, "float32 must be 4 bytes");
+static_assert(sizeof(float64) == 8, "float64 must be 8 bytes");
 
 using byte  = std::byte;
 using usize = uint64;
 
 using intxx = mpz_class;
+using floatxx = mpf_class;
 
 constexpr usize EXPANDABLE_ARRAY = 0;
 
